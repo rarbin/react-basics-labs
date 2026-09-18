6408ba5614a195fb2c7df132e9a4cef3616d3219
Rob V <rvenn.colab@gmail.com>
2026-09-18 16:31:12 +0100

    Add task: form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import Task from './components/Task';
 import React, { useState } from 'react';
-
+import AddTaskForm from './components/Form';
 
 function App() {
 
@@ -41,6 +41,7 @@ function App() {
           deleteTask = {() => deleteHandler(index)}
           />
       ))}
+      <AddTaskForm />
     </div>
 
   );
