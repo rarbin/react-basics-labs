b461bc9e83d95fe894165c5e4c380314c9ccb285
Rob V <rvenn.colab@gmail.com>
2026-09-18 16:06:12 +0100

    Mark task as done

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -1,10 +1,11 @@
 const Task = (props) => {
         return (
-        <div className="card">
+        <div className="card" style={{backgroundColor: props.done ? '#9abc34' : '#f0904b'}}>
             <p className="title">{props.title}</p>
             <p className="deadline">Due: {props.deadline}</p>
             <p className="description">{props.description}</p>
             <p className="priority">{props.priority}</p>
+            <button onClick={props.markDone} className='doneButton'>Done</button>
         </div>
     )
 
